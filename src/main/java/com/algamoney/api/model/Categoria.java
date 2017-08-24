4ef1b9c0f002d6506c9230d3635e3321a0b280b9
Eduardo Branquinho <edubranquinho@gmail.com>
2017-08-23 23:04:04 -0300

    recurso buscar categorias

---
+++ src/main/java/com/algamoney/api/model/Categoria.java
@@ -0,0 +1,44 @@
+package com.algamoney.api.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "categoria")
+public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long codigo;
+    private String nome;
+
+    public Long getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(Long codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Categoria categoria = (Categoria) o;
+
+        return codigo != null ? codigo.equals(categoria.codigo) : categoria.codigo == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return codigo != null ? codigo.hashCode() : 0;
+    }
+}
