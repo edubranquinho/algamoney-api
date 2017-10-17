ff7b6da5b19bc292e61f27e61cff095442cd5dbc
Eduardo Branquinho <edubranquinho@gmail.com>
2017-10-16 23:19:47 -0200

    Iniciando recurso de lancamento

--- src/main/java/com/algamoney/api/AlgamoneyApiApplication.java
+++ src/main/java/com/algamoney/api/AlgamoneyApiApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AlgamoneyApiApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(AlgamoneyApiApplication.class, args);
 	}
